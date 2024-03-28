17cfb2b645215b717deb6abde0f9a580dc37ed24
SOME Barsour Evence <sourbar2001@gmail.com>
2024-03-28 13:45:14 +0100

    admin controllers

---
+++ controllers/Admin.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcrypt');
+
+const admin = require('../models/Admin');
+
+const jwt = require('jsonwebtoken');
+
+exports.signup = (req, res, next) => {
+    bcrypt.hash(req.body.password, 10)
+      .then(hash => {
+        const admin = new admin({
+          speudo: req.body.speudo,
+          mdp: hash
+        });
+        admin.save()
+          .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+          .catch(error => res.status(400).json({ error }));
+      })
+      .catch(error => res.status(500).json({ error }));
+  };
+
+  exports.login = (req, res, next) => {
+    admin.findOne({ speudo: req.body.speudo })
+        .then(admin => {
+            if (!admin) {
+                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
+            }
+            bcrypt.compare(req.body.mdp, admin.mdp)
+                .then(valid => {
+                    if (!valid) {
+                        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                    }
+                    res.status(200).json({
+                        adminId: admin._id,
+                        token: jwt.sign(
+                            { adminId: admin._id },
+                            'RANDOM_TOKEN_SECRET',
+                            { expiresIn: '24h' }
+                        )
+                    });
+                })
+                .catch(error => res.status(500).json({ error }));
+        })
+        .catch(error => res.status(500).json({ error }));
+ };
